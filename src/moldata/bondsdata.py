04877ae979257d386569081cf4c7edcbd75b2d7d
Toon Verstraelen <Toon.Verstraelen@UGent.be>
2005-08-17 09:57:27 +0200

    Added documentation and renamed a variable.

--- src/moldata/bondsdata.py
+++ src/moldata/bondsdata.py
@@ -33,6 +33,10 @@ BOND_TRIPLE = 3
 bond_types = [BOND_SINGLE, BOND_DOUBLE, BOND_TRIPLE]
 
 class BondData(object):
+    """This object loads information about average bond lengths from a csv file
+       when it is initialized. Missing data points in the csv file are estimated
+       by adding Van der Waals radii of the two atoms of the given bond.
+    """
     bond_tolerance = 1.2
 
     def __init__(self, filename, periodic_data):
@@ -43,6 +47,8 @@ class BondData(object):
         self.max_length = max(self.bond_lengths.itervalues())
         
     def read_length(self, BOND_TYPE, cells, col):
+        """This is a helper method for load_bond_data.
+        """
         for i in range((len(cells) - 4) / 3):
             cell = cells[col + 3 + i*3]
             if cell != 'NA':
@@ -50,6 +56,13 @@ class BondData(object):
                 return
                 
     def load_bond_data(self, filename):
+        """It's assumed that the uncommented lines in the data file have the
+           following format:
+           "symbol1 symbol2 number1 number2 bond_length_single_a\
+            bond_length_double_a bond_length_triple_a bond_length_single_b\
+            bond_length_double_b bond_length_triple_b ..."
+           where a, b, ... stand for different sources.
+        """
         bond_file = file(filename)
         for line in bond_file:
             cells = string.split(line)
@@ -62,31 +75,41 @@ class BondData(object):
         bond_file.close()
 
     def approximate_unkown_bond_lengths(self):
+        dataset = self.bond_lengths[BOND_SINGLE]
         for index1, n1 in enumerate(self.periodic_data.numbers):
             for n2 in self.periodic_data.numbers[index1+1:]
                 pair = frozenset([n1, n2])
-                if pair not in self.bond_lengths:
-                    self.bond_lengths[pair] = self.periodic_data.radius[n1] + self.periodic_data.radius[n2]
+                if pair not in dataset:
+                    dataset[pair] = self.periodic_data.radius[n1] + self.periodic_data.radius[n2]
                 
-    def bonded(self, n1, n2, length):
-        if length > self.max_length * self.bond_tolerance: return None
+    def bonded(self, n1, n2, distance):
+        """This method checks wether for the given pair of atom numbers, the
+           given distance corresponds to a certain bond_length. The best
+           matching bond type will be returned. If the distance is a factor
+           self.bond_tolerance larger than a tabulated distance, the algorithm
+           will not relate them.
+        """
+        if distance > self.max_length * self.bond_tolerance: return None
         deviation = 0.0
         pair = frozenset(n1,n2)
         bond_type = None
         for bt in bond_types:
             bond_length =  self.bond_lengths[bt][pair]
-            if length < bond_length * self.bond_tolerance:
+            if distance < bond_length * self.bond_tolerance:
                 if bond_type == None:
                     bond_type = bt
-                    deviation = abs(bond_length - length)
+                    deviation = abs(bond_length - distance)
                 else:
-                    new_deviation = abs(bond_length - length)
+                    new_deviation = abs(bond_length - distance)
                     if deviation > new_deviation:
                         bond_type = bt
                         deviation = new_deviation
         return bond_type
         
     def get_length(self, n1, n2, bond_type):
+        """This is a safe method for querying a bond_length. If no answer can be
+           found, this get_length returns None.
+        """
         dataset = self.bond_lengths.get(bond_type)
         if dataset == None:
             return None
