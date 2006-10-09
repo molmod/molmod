8081ca2edee866828d1f2a15691a663332cbbed4
Toon Verstraelen <Toon.Verstraelen@UGent.be>
2006-10-09 16:56:08 +0200

    Enhanced ESP cost function.

--- src/esp.py
+++ src/esp.py
@@ -24,21 +24,37 @@ import numpy
 
 
 class ESPCostFunction(object):
-    def __init__(self, potential_data, charges, dipoles, external_potential=None):
-        design_matrix = numpy.zeros((len(potential_data), len(charges) + len(dipoles)*3), float)
-        expected_values = potential_data[:,-1].copy()
+    def __init__(self, potential_data, charges, dipoles, weights=None, external_potential=None, epsilons=None):
+        if potential_data is not None:
+            self.charges = charges
+            self.dipoles = dipoles
 
-        if external_potential is not None:
-            expected_values -= external_potential(potential_data[:,:-1])
+            design_matrix = numpy.zeros((len(potential_data), len(charges) + len(dipoles)*3), float)
+            expected_values = potential_data[:,-1].copy()
 
-        for row, record in enumerate(potential_data):
-            point = record[:-1]
-            design_matrix[row,:len(charges)] = 1/numpy.sqrt(sum((point - charges).transpose()**2))
-            design_matrix[row,len(charges):] = ((point - dipoles).transpose()/numpy.sqrt(sum((point - dipoles).transpose()**2)**3)).transpose().ravel()
+            if external_potential is not None:
+                expected_values -= external_potential(potential_data[:,:-1])
 
-        self.A = numpy.dot(design_matrix.transpose(), design_matrix)
-        self.B = numpy.dot(design_matrix.transpose(), expected_values)
-        self.C = numpy.dot(expected_values, expected_values)
+            for row, record in enumerate(potential_data):
+                point = record[:-1]
+                if len(charges) > 0:
+                    design_matrix[row,:len(charges)] = 1/numpy.sqrt(sum((point - charges).transpose()**2))
+                if len(dipoles) > 0:
+                    design_matrix[row,len(charges):] = ((point - dipoles).transpose()/numpy.sqrt(sum((point - dipoles).transpose()**2)**3)).transpose().ravel()
+
+            if weights is not None:
+                expected_values *= weights
+                tmp = design_matrix.transpose()
+                tmp *= weights
+
+            self.A = numpy.dot(design_matrix.transpose(), design_matrix)
+            self.B = numpy.dot(design_matrix.transpose(), expected_values)
+            self.C = numpy.dot(expected_values, expected_values)
+
+            if epsilons is not None:
+                self.A[::len(self.A)+1] += epsilons
+
+
 
     def evaluate(self, unknowns):
         return numpy.dot(numpy.dot(unknowns, self.A) - 2*self.B, unknowns) + self.C
@@ -49,3 +65,12 @@ class ESPCostFunction(object):
     def solve(self):
         return numpy.linalg.solve(self.A, self.B)
 
+    def solve_constrained(self):
+        projection = numpy.array((len(self.charges) + len(self.dipoles) - 1, len(self.charges) + len(self.dipoles)), float)
+        projection[0:len(charges),0] = -1
+        for index in xrange(len(charges) + len(dipoles) - 1):
+            projection[index+1, index] = 1
+
+        A = numpy.dot(projection, numpy.dot(self.A, projection.transpose()))
+        B = numpy.dot(projection, B)
+        return numpy.dot(projection.transpose(), numpy.linalg.solve(A, B))
