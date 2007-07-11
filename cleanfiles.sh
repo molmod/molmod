af13822ff61fb1441b2ae8df497f9fa70cb559a7
Toon Verstraelen <Toon.Verstraelen@UGent.be>
2007-07-11 15:25:28 +0200

    Updated cleanfiles, now also removes debian cruft.

--- cleanfiles.sh
+++ cleanfiles.sh
@@ -1,5 +1,12 @@
 for i in `find * | egrep "\.pyc$|\.py~$|\.pyc~$|\.bak$"` ; do rm -v ${i}; done
 
+rm -vr debian/python-*
+rm -vr debian/pycompat
+rm -vr debian/compat
+rm -vr debian/files
+rm -vr debian/stamp-makefile-build
+rm -vr python-build-stamp-* 
+
 rm -vr test/output
 rm -v test/molmod
 
