de02609c2d477dd802dbf3ea597883a8f377d6b2
Toon Verstraelen <Toon.Verstraelen@UGent.be>
2008-03-18 10:39:33 +0100

    Updated clean scripts.

--- cleancode.sh
+++ cleancode.sh
@@ -1,5 +1,5 @@
 echo Cleaning python code in \'`pwd`\' and subdirectories
-for file in `find * | egrep "(\.py$)|(\.f90$)|(^scripts/tr-)"`; do
+for file in `find * | egrep "(\.py$)|(\.c$)|(\.h$)|(\.i$)"`; do
   echo Cleaning ${file}
   sed -i -e $'s/\t/    /' ${file}
   sed -i -e $'s/[ \t]\+$//' ${file}
