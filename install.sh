6c8a771fffcc1424028d6d88f84627f22e164823
Toon Verstraelen <Toon.Verstraelen@UGent.be>
2009-04-29 10:23:09 +0200

    Default install in home dir

--- install.sh
+++ install.sh
@@ -1,8 +1,16 @@
 #! /bin/sh
 # This is a very simplistic install scipt. Use with care!
 
-./uninstall.sh
-(cd ext; python setup.py install)
-python setup.py install
+if [ -z $1 ] && [ "$1" = "--system" ]; then
+  ./uninstall.sh --system
+  (cd ext; python setup.py install)
+  python setup.py install
+else
+  ./uninstall.sh
+  (cd ext; python setup.py install --home=$HOME)
+  python setup.py install --home=$HOME
+  echo "Don't forget to add 'export PYTHONPATH=$HOME' to your .bashrc file."
+fi
 ./cleanfiles.sh
 (cd ext; ./cleanfiles.sh)
+
--- uninstall.sh
+++ uninstall.sh
@@ -1,5 +1,10 @@
 #! /bin/sh
 # This is a very simplistic uninstall scipt. Use with care!
 
-rm -vr /usr/share/molmod
-rm -vr /usr/lib/python*/site-packages/molmod
+if [ -z $1 ] && [ "$1" = "--system" ]; then
+  rm -vr /usr/share/molmod
+  rm -vr /usr/lib/python*/site-packages/molmod
+else
+  rm -vr $HOME/share/molmod
+  rm -vr $HOME/lib/molmod
+fi
