a703bc83bb77d32bb2504cd67f7686931e4396c8
Toon Verstraelen <Toon.Verstraelen@UGent.be>
2011-01-13 13:28:03 +0100

    New CubeFile implementation

--- molmod/io/cube.py
+++ molmod/io/cube.py
@@ -23,10 +23,54 @@
 """Reader for the cube format"""
 
 
-import numpy
+import numpy as np
+
+from molmod.molecules import Molecule
 
 
-__all__ = ["CubeReader"]
+__all__ = ["CubeReader", 'CubeFile']
+
+
+
+def read_cube_header(f):
+    # skip the first two lines
+    title = f.readline().strip()
+    subtitle = f.readline().strip()
+
+    def read_grid_line(line):
+        """Read a grid line from the cube file"""
+        words = line.split()
+        return (
+            int(words[0]),
+            np.array([float(words[1]), float(words[2]), float(words[3])], float)
+            # all coordinates in a cube file are in atomic units
+        )
+
+    # number of atoms and origin of the grid
+    natom, origin = read_grid_line(f.readline())
+    # numer of grid points in A direction and step vector A, and so on
+    nrep0, axis0 = read_grid_line(f.readline())
+    nrep1, axis1 = read_grid_line(f.readline())
+    nrep2, axis2 = read_grid_line(f.readline())
+    nrep = np.array([nrep0, nrep1, nrep2], int)
+    axes = np.array([axis0, axis1, axis2])
+
+    def read_coordinate_line(line):
+        """Read an atom number and coordinate from the cube file"""
+        words = line.split()
+        return (
+            int(words[0]),
+            np.array([float(words[2]), float(words[3]), float(words[4])], float)
+            # all coordinates in a cube file are in atomic units
+        )
+
+    numbers = np.zeros(natom, int)
+    coordinates = np.zeros((natom, 3), float)
+    for i in xrange(natom):
+        numbers[i], coordinates[i] = read_coordinate_line(f.readline())
+
+    molecule = Molecule(numbers, coordinates, title=title)
+    return molecule, origin, axes, nrep, subtitle
 
 
 class CubeReader(object):
@@ -47,43 +91,13 @@ class CubeReader(object):
             | ``filename``  --  the filename with the formatted cube data
         """
         self.f = file(filename)
-        # skip the first two lines
-        self.f.readline()
-        self.f.readline()
 
-        def read_grid_line(line):
-            """Read a grid line from the cube file"""
-            words = line.split()
-            return (
-                int(words[0]),
-                numpy.array([float(words[1]), float(words[2]), float(words[3])], float)
-                # all coordinates in a cube file are in atomic units
-            )
+        self.molecule, self.origin, self.axes, self.nrep, self.subtitle = \
+            read_cube_header(self.f)
 
-        # number of atoms and origin of the grid
-        self.num_atoms, self.origin = read_grid_line(self.f.readline())
-        # numer of grid points in A direction and step vector A, and so on
-        self.num_a, self.vector_a = read_grid_line(self.f.readline())
-        self.num_b, self.vector_b = read_grid_line(self.f.readline())
-        self.num_c, self.vector_c = read_grid_line(self.f.readline())
-
-        def read_coordinate_line(line):
-            """Read an atom number and coordinate from the cube file"""
-            words = line.split()
-            return (
-                int(words[0]),
-                numpy.array([float(words[2]), float(words[3]), float(words[4])], float)
-                # all coordinates in a cube file are in atomic units
-            )
-
-        self.numbers = numpy.zeros(self.num_atoms, int)
-        self.coordinates = numpy.zeros((self.num_atoms, 3), float)
-        for i in xrange(self.num_atoms):
-            self.numbers[i], self.coordinates[i] = read_coordinate_line(self.f.readline())
-
-        self._counter_a = 0
-        self._counter_b = 0
-        self._counter_c = 0
+        self._counter0 = 0
+        self._counter1 = 0
+        self._counter2 = 0
         self._done = False
         self._values = []
 
@@ -106,16 +120,83 @@ class CubeReader(object):
                 raise StopIteration
             self._values = [float(word) for word in line.split()]
         value = self._values.pop(0)
-        vector = self.origin + self._counter_a*self.vector_a \
-                             + self._counter_b*self.vector_b \
-                             + self._counter_c*self.vector_c
-        self._counter_c += 1
-        if self._counter_c >= self.num_c:
-            self._counter_c = 0
-            self._counter_b += 1
-            if self._counter_b >= self.num_b:
-                self._counter_b = 0
-                self._counter_a += 1
-                if self._counter_a >= self.num_a:
+        vector = self.origin + self._counter0*self.axes[0] \
+                             + self._counter1*self.axes[1] \
+                             + self._counter2*self.axes[2]
+        self._counter2 += 1
+        if self._counter2 >= self.nrep[2]:
+            self._counter2 = 0
+            self._counter1 += 1
+            if self._counter1 >= self.nrep[1]:
+                self._counter1 = 0
+                self._counter0 += 1
+                if self._counter0 >= self.nrep[0]:
                     self._done = True
         return vector, value
+
+
+class CubeFile(object):
+    @classmethod
+    def from_file(cls, filename):
+        f = file(filename)
+        molecule, origin, axes, nrep, subtitle = read_cube_header(f)
+        data = np.zeros(tuple(nrep), float)
+        tmp = data.ravel()
+        counter = 0
+        while True:
+            line = f.readline()
+            if len(line) == 0:
+                break
+            words = line.split()
+            for word in words:
+                tmp[counter] = float(word)
+                counter += 1
+        f.close()
+        return cls(molecule, origin, axes, nrep, data, subtitle)
+
+    def __init__(self, molecule, origin, axes, nrep, data, subtitle=''):
+        self.molecule = molecule
+        self.origin = np.array(origin, copy=False)
+        self.axes = np.array(axes, copy=False)
+        self.nrep = np.array(nrep, copy=False)
+        self.data = np.array(data, copy=False)
+        self.subtitle = subtitle
+
+    def write_to_file(self, fn):
+        f = open(fn, 'w')
+        print >> f, ' ' + self.molecule.title
+        print >> f, ' ' + self.subtitle
+
+        def write_grid_line(n, v):
+            print >> f, '%5i % 11.6f % 11.6f % 11.6f' % (n, v[0], v[1], v[2])
+
+        write_grid_line(self.molecule.size, self.origin)
+        write_grid_line(self.data.shape[0], self.axes[0])
+        write_grid_line(self.data.shape[1], self.axes[1])
+        write_grid_line(self.data.shape[2], self.axes[2])
+
+        def write_atom_line(n, v):
+            print >> f, '%5i % 11.6f % 11.6f % 11.6f % 11.6f' % (n, n, v[0], v[1], v[2])
+
+        for i in xrange(self.molecule.size):
+            write_atom_line(self.molecule.numbers[i], self.molecule.coordinates[i])
+
+        for i0 in xrange(self.data.shape[0]):
+            for i1 in xrange(self.data.shape[1]):
+                col = 0
+                for i2 in xrange(self.data.shape[2]):
+                    value = self.data[i0, i1, i2]
+                    if col % 6 == 0:
+                        print >> f, ' % 12.5e' % value,
+                    elif col % 6 == 5:
+                        print >> f, '% 12.5e' % value
+                    else:
+                        print >> f, '% 12.5e' % value,
+                    col += 1
+                if col % 6 != 5:
+                    print >> f
+        f.close()
+
+    def copy(self):
+        return self.__class__(self.molecule, origin.copy(), axes.copy(),
+                              nrep.copy(), data.copy(), self.subtitle)
